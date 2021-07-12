558f22b21dccd791e60809e2088ac9988584ad1a
vincent <vincentuong@yahoo.com>
2021-07-11 23:35:46 -0500

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -7,10 +7,7 @@ class MusicLibraryController
     end
 
     def call
-        input = gets.chomp
-        
-        while input != exit
-        
+
         puts "Welcome to your music library!"
         puts "To list all of your songs, enter 'list songs'."
         puts "To list all of the artists in your library, enter 'list artists'."
@@ -20,6 +17,11 @@ class MusicLibraryController
         puts "To play a song, enter 'play song'."
         puts "To quit, type 'exit'."
         puts "What would you like to do?"
+            
+        input = gets.chomp
+        
+        while input != exit
+
 
         case input
             when "list songs"
